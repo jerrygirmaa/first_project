6c047883c2eb5adad281ebbdd890aac2f7694bfc
jerrygirmaa <jerihagbj@gmail.com>
2024-10-09 14:59:23 +0300

    hjjh

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
-
 class UserController extends Controller
 {
    public function login(Request $request){
